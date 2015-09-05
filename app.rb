1f5e2be855fe66c65a99ed7aba89cf6d46223b3a
Denis Doan <dedoin08@yahoo.de>
2015-09-05 14:29:29 +0100

    New Page done and the layout is in process

--- app.rb
+++ app.rb
@@ -11,8 +11,16 @@ after do
   @db.close
 end
 
+# Index
 get '/' do
   sql = "SELECT * FROM videos"
   @videos = @db.exec(sql)
   erb :index
+end
+
+# New
+get '/new' do
+  sql = "insert into videos (title, description, url, genre, year) values('#{@title}', '#{@description}', '#{@url}', '#{@genre}', '#{@year}')"
+    @db.exec(sql)
+  erb :create
 end
